7e761395b62940876f1945a78d2555d60ea42403
hafiz8315000 <hafiz8315000@gmail.com>
2026-09-03 01:46:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello from Cloudflare Workers!");
-  },
-};
+import express from "express";
+import { httpServerHandler } from "cloudflare:node";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express is working on Cloudflare!");
+});
+
+export default httpServerHandler({ port: 3000, handler: app });
